1de97cb2eb4e88da049fbe05fa7f29f154cd06e8
taikitominari <taikitominari.co.jp@icloud.com>
2021-08-02 22:43:49 +0900

    APIからデータを取得する

--- exercises/js/main.js
+++ exercises/js/main.js
@@ -1,3 +1,38 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        items: null,
+        keyword: '',
+        message: ''
+    },
+    watch: {
+
+    },
+    created: function() {
+        this.keyword = 'JavaScript'
+        this.getAnswer()
+    },
+    methods: {
+        getAnswer: function() {
+            if(this.keyword === '' ) {
+                this.item = null
+                return
+            }
+
+            this.message = 'Loading...'
+            let vm = this
+            let params = { page: 1, per_page: 20, query: this.keyword }
+            axios.get('https://qiita.com/api/v2/items' , { params })
+            .then(function(response) {
+                console.log(response)
+                vm.items = response.data
+            })
+            .catch(function(error) {
+                vm.message = 'Error!' + error
+            })
+            .finally(function() {
+                vm.message = ''
+            })
+        }
+    }
 })
