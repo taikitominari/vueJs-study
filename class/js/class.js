8e03761658b407b81a264bc071eae7b0533b2d3f
taikitominari <taikitominari.co.jp@icloud.com>
2021-08-10 11:56:02 +0900

    配列によるクラスの適応

--- class/js/class.js
+++ class/js/class.js
@@ -1,15 +1,7 @@
 let app = new Vue({
   el: '#app',
   data: {
-    isLarge: false,
-    hasError: false
-  },
-  methods: {
-    colorBtn: function() {
-      this.isLarge = !this.isLarge
-    },
-    sizeBtn: function() {
-      this.hasError = !this.hasError
-    }
+    largeClass: 'large',
+    dangerClass: 'text-danger'
   }
 })
